0b11cce67abe025fdc39c78174665b65007c3562
Taylor Dolezal <onlydole@gmail.com>
2020-09-15 10:37:54 -0700

    Rename dvo to certificate

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "aws_acm_certificate" "this" {
 
 resource "aws_route53_record" "this" {
   for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
+    for certificate in aws_acm_certificate.this.domain_validation_options : certificate.domain_name => {
+      name   = certificate.resource_record_name
+      record = certificate.resource_record_value
+      type   = certificate.resource_record_type
     }
   }
 
