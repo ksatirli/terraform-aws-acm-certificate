9147f4fc981236e9102cf6ee88d18bc56e436a7a
Kerim Satirli <k.satirli@gmail.com>
2022-08-26 21:17:45 +0200

    linting

--- examples/basic/providers.tf
+++ examples/basic/providers.tf
@@ -4,7 +4,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#
+# The AWS Provider is set to retrieve configuration from the executing environment
+# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#schema
+# This aliased version of the provider is specifically limited to the `us-east-1`
+# region to allow for lifecycle operations for ACM resources.
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
